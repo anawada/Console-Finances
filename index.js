abd4342a1f80b661d9fbd64953628f13c3bc45ed
anawada <anamitsuko@hotmail.com>
2022-12-16 10:32:00 +0000

    results alert on the page

--- index.js
+++ index.js
@@ -90,17 +90,36 @@ var finances = [
 // The total number of months included in the dataset.
 
 let totalMonths = finances.length;
-console.log(totalMonths);
+console.log(totalMonths)
 
 // The net total amount of Profit/Losses over the entire period.
 
+    for (let i = 0; i < finances.length; i++) {
+        const number = finances[i];
+
+
+        
+    }
+
 // The average of the changes in Profit/Losses over the entire period.
 
-// You will need to track what the total change in profits are from month to month and then find the average.
+
+
+// You will need to track what the total change in profits are from month to month and then 
+//find the average.
+
 // (Total/Number of months)
+
 // The greatest increase in profits (date and amount) over the entire period.
 
 // The greatest decrease in losses (date and amount) over the entire period.
 
-
-//
+alert(`
+Financial Analysis
+----------------------------
+Total Months: ${totalMonths}
+Total: $2561231
+Average  Change: $-2315.12
+Greatest Increase in Profits: Feb-2012 ($1926159)
+Greatest Decrease in Profits: Sep-2013 ($-2196167)
+`);
